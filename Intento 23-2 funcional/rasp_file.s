02d8c5727ab67dc14a513b266905a3f802598924
Bryan Jiménez Chacón <bsjc96@gmail.com>
2017-02-23 20:29:49 -0600

    Proyecto Funcional?
    # Conflicts:
    #	Avances-20-2-2017/Main.c
    #	Avances-20-2-2017/global.h
    #	Avances-20-2-2017/mips_file.asm
    #	Avances-20-2-2017/parser.c
    #	Avances-20-2-2017/parser.h
    #	Avances-20-2-2017/prueba1.txt
    #	Avances-20-2-2017/prueba2.txt
    #	Avances-20-2-2017/scanner.c
    #	Avances-20-2-2017/scanner.h
    #	Inicio/Main
    #	Inicio/README.md
    #	Inicio/output_file.txt
    #	Inicio/prueba3.txt
    #	Inicio/translator.c
    #	Inicio/translator.h
    #	Intento 23-2 funcional/Main.c
    #	Intento 23-2 funcional/README.md
    #	Intento 23-2 funcional/global.h
    #	Intento 23-2 funcional/mips_file.asm
    #	Intento 23-2 funcional/p4.txt
    #	Intento 23-2 funcional/parser.h
    #	Intento 23-2 funcional/prueba1.txt
    #	Intento 23-2 funcional/prueba2.txt
    #	Intento 23-2 funcional/scanner.c
    #	Intento 23-2 funcional/scanner.h
    #	Intento 23-2 funcional/translator.h
    #	Intento/Main.c
    #	Intento/README.md
    #	Intento/global.h
    #	Intento/mips_file.asm
    #	Intento/parser.c
    #	Intento/parser.h
    #	Intento/prueba1.txt
    #	Intento/prueba2.txt
    #	Intento/prueba3.txt
    #	Intento/scanner.c
    #	Intento/scanner.h
    #	Intento/translator.h
    #	Intentos Anteriores/Intento 21-2/parser.c

---
+++ Intento 23-2 funcional/rasp_file.s
@@ -0,0 +1,44 @@
+.data
+.balign 4
+    string: .asciz "Result: %d"
+    A: .word 512 
+    B: .word 512 
+    Temp1: .word 512 
+
+
+.text
+.global main
+.extern printf
+
+main:
+
+@Soy un store
+    LDR R0, =18
+    LDR R8, =A
+    STR R0,[R8]
+
+@Soy un add
+    LDR R7, =A
+    LDR R1,[R7]
+    LDR R2, =2
+    ADD R3, R1, R2
+  LDR R8,=Temp1
+    STR R3,[R8]
+@Soy un store
+    LDR R0,=Temp1
+    LDR R7,[R0]
+    LDR R8, =B
+    STR R7,[R8]
+
+@Soy un Write
+    push{ip, lr}
+    LDR R8,=B
+    LDR R1,[R8]
+    ldr R0,=string
+    bl printf
+    pop{ip,pc}
+@Soy un Halt
+    mov r7, #1 
+    mov r0, #0
+    svc #0
+    bx lr
